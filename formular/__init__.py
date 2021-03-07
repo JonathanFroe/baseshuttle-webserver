a9e7fcd049bd559737e3a78b4a949fb4ee50d7bb
JonathanFroe <57289773+JonathanFroe@users.noreply.github.com>
2021-03-07 15:23:48 +0100

    save data to csv

--- formular/__init__.py
+++ formular/__init__.py
@@ -1,24 +1,27 @@
 import logging
+import uuid
 
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, redirect
 
 formular = Blueprint('formular', __name__, template_folder='templates',
                                static_folder='static', url_prefix='/formular')
 
 @formular.route('/', methods=['GET', 'POST'])
-def main():
+def home():
     if request.method == 'POST':
+        user_id = uuid.uuid4()
         email = request.form['email']
         time = request.form['time']
         firstname = request.form['firstname']
-        lastname = request.fornm['lastname']
+        lastname = request.form['lastname']
+        companions = request.form['companions']
         if time == 1:
-            with open('data/time_one.csv', 'w') as file:
-                file.write(";".join([email,firstname,lastname]))              
+            with open('formular/data/time_one.csv', 'w') as file:
+                file.write(";".join([user_id,email,firstname,lastname,companions]))              
         else:
-            with open('data/time_two.csv', 'w') as file:
-                file.write(";".join([email,firstname,lastname]))
-        
+            with open('formular/data/time_two.csv', 'w') as file:
+                file.write(";".join([user_id, email,firstname,lastname, companions]))
         return render_template('formular.html')
-        
     return render_template('formular.html')
+
+@formular
